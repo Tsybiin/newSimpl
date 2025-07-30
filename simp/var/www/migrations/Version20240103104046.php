fcc65e35f46da9c1933e772ae0358d9b006cded5
Tsybiin <tsybin@inventive.ru>
2025-07-30 21:54:39 +0300

    add migration

--- simp/var/www/migrations/Version20240103104046.php
+++ simp/var/www/migrations/Version20240103104046.php
@@ -14,18 +14,49 @@ final class Version20240103104046 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'create table';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        // user
+         $this->addSql('create table user
+(
+    ID INT AUTO_INCREMENT NOT NULL
+        primary key,
+    ID_TELEGRAM    INT     null,
+    ID_CHAT_TELEGRAM    INT     null,
+    ID_OPEN_VPN_KEY INT          null,
+    DATE_REGISTER DATETIME DEFAULT NULL
+)
+    collate = utf8mb3_unicode_ci');
+
+        // key_vpn
+         $this->addSql('create table key_vpn
+(
+    ID INT AUTO_INCREMENT NOT NULL
+        primary key,
+    NAME    VARCHAR(255) NOT NULL,
+    DATE_SEND DATETIME DEFAULT NULL
+)
+    collate = utf8mb3_unicode_ci');
+
+        // log_vpn_status
+        $this->addSql('create table  log_vpn_status
+(
+    ID INT AUTO_INCREMENT NOT NULL
+        primary key,
+    NAME    VARCHAR(255) NOT NULL,
+    DATE_UPDATE DATETIME DEFAULT NULL
+)
+    collate = utf8mb3_unicode_ci');
     }
+
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE log_vpn_status');
+        $this->addSql('DROP TABLE key_vpn');
     }
 }
